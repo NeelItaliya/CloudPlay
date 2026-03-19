1f0ff51589ee77cf573ff8b6438f699aadb8d50d
Neel Italiya <neelitaliya@Neels-MacBook-Air.local>
2026-03-19 13:58:10 +0530

    feat: update AMI to cloudplay-backend-v3 with DynamoDB

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,7 +10,7 @@ variable "vpc_id" {
 
 variable "ami_id" {
   description = "AMI ID for launch template — use cloudplay-backend-v2 AMI ID"
-  default     = "ami-0040191e9841397bd"
+  default     = "ami-0332769a9fb161222"
 }
 
 variable "instance_type" {
